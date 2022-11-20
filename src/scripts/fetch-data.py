41a99b35524392e874ed8f5dc8d2894cdc907fb7
nico-koltermann <nico.koltermann@get-racing.de>
2022-11-20 21:47:24 +0000

    Connect script to db

--- src/scripts/fetch-data.py
+++ src/scripts/fetch-data.py
@@ -1,7 +1,12 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+import sqlite3
+
+import json
+
 # Handling time and unix timestamp
+import os
 import time
 import datetime
 import dateutil.parser
@@ -11,59 +16,95 @@ from dotenv import dotenv_values
 
 def fetch_data():
 
-    config = dotenv_values(".env")
+    path_ws = os.path.dirname(os.path.abspath(__file__))
 
-    scope = "user-read-recently-played"
+    config = dotenv_values(path_ws + '/.env')
 
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth( client_id=config.get("SPOTIPY_CLIENT_ID"),
-                                                    client_secret=config.get("SPOTIPY_CLIENT_SECRET"),
-                                                    redirect_uri=config.get("SPOTIPY_REDIRECT_URI"),
+    scope = 'user-read-recently-played'
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth( client_id=config.get('SPOTIPY_CLIENT_ID'),
+                                                    client_secret=config.get('SPOTIPY_CLIENT_SECRET'),
+                                                    redirect_uri=config.get('SPOTIPY_REDIRECT_URI'),
                                                     scope=scope))
 
     # Fetch last 50 played songs
     results = sp.current_user_recently_played(limit=50)
 
-    # Last saved timestamp
-    f = open("last-stamp.save", "r")
-    stamp = f.read()
+    # create a database connection
+    conn = create_connection(path_ws + '/../database/spotify.db')
+    
+    stamp = get_last_timestamp(conn)
 
-    # Save new data in seperate string
-    newData = ''
+    # Revert order, so last is latest
+    items = results['items']
+    items = items[::-1]
 
-    for idx, item in enumerate(results['items']):
+    for idx, item in enumerate(items):
 
-        # only take songs, older then last timestamp
-        if  int(dateutil.parser.parse(item["played_at"]).strftime('%s')) > int(stamp):
+        # Only take songs, older then last timestamp
+        if  int(dateutil.parser.parse(item['played_at']).strftime('%s')) > int(stamp):
 
             track = item['track']
-            newData = newData + track['artists'][0]['name'] + " - " + track['name'] + "\n"
+            
+            with conn:
 
-        # At index zero, the last 
-        if idx == 0:
-            last_stamp = item["played_at"]
+                track_json = json.dumps(track)
 
-    # Write new data at beginning of existing file
-    with open('history.txt', 'r') as original: data = original.read()
-    original.close()
-    with open('history.txt', 'w') as modified: modified.write(newData + data)
-    modified.close()
+                task = ( 
+                    track['artists'][0]['name'],
+                    track['name'],
+                    track_json,
+                    str(dateutil.parser.parse(item['played_at']).strftime('%s')))
 
-    # Save last time stamp from data
-    # Format out of api:
-    # 2022-11-18T23:09:04.344Z
-    d = dateutil.parser.parse(last_stamp).strftime('%s')
-    f = open("last-stamp.save", "w")
-    f.write(str(d))
-    f.close()
+                create_task(conn, task)
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    conn = sqlite3.connect(db_file)
+
+    return conn
+
+def get_last_timestamp(conn):
+    """ Get latest timesstamp from db
+    :param db_file: database file
+    :return: Connection object or None
+    """
+
+    sql = ''' SELECT MAX(history_stamp) FROM history; '''
+    cur = conn.cursor()
+    max_stamp = cur.execute(sql).fetchone()[0]
+    return max_stamp
+    
+def create_task(conn, task):
+    """
+    Create a new task
+    :param conn:
+    :param task:
+    :return:
+    """
+
+    sql = ''' INSERT INTO history(artist, song, history_entry, history_stamp)
+              VALUES(?,?, ?, ?) '''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
+    return cur.lastrowid
 
 if __name__ == '__main__':
 
-    # If somethign went wrong, write into new logfile
+    # If something went wrong, write into new logfile
     try:
         fetch_data()
-    except e:
-        f = open('logs/' + str(datetime.now().strftime("%H:%M:%S")) +  "_error.log", "w") 
-        f.write(e)
+    except Exception as e:
+        path_ws = os.path.dirname(os.path.abspath(__file__))
+        f = open(path_ws + '/logs/' + str(datetime.datetime.now().strftime('%H:%M:%S')) +  '_error.log', 'w') 
+        f.write(str(e))
         f.close()
 
 
