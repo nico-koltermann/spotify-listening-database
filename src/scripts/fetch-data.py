f584797432333e0da066f8d5f01e0580e6a29a77
nico-koltermann <nico.koltermann@get-racing.de>
2022-11-20 19:33:09 +0000

    Add logging at errors

--- src/scripts/fetch-data.py
+++ src/scripts/fetch-data.py
@@ -1,48 +1,70 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-from dotenv import dotenv_values
-
+# Handling time and unix timestamp
 import time
 import datetime
 import dateutil.parser
 
-config = dotenv_values(".env")
+# Load env variables
+from dotenv import dotenv_values
 
-scope = "user-read-recently-played"
+def fetch_data():
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth( client_id=config.get("SPOTIPY_CLIENT_ID"),
-                                                client_secret=config.get("SPOTIPY_CLIENT_SECRET"),
-                                                redirect_uri=config.get("SPOTIPY_REDIRECT_URI"),
-                                                scope=scope))
+    config = dotenv_values(".env")
 
-results = sp.current_user_recently_played(limit=50)
+    scope = "user-read-recently-played"
 
-f = open("last-stamp.save", "r")
-stamp = f.read()
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth( client_id=config.get("SPOTIPY_CLIENT_ID"),
+                                                    client_secret=config.get("SPOTIPY_CLIENT_SECRET"),
+                                                    redirect_uri=config.get("SPOTIPY_REDIRECT_URI"),
+                                                    scope=scope))
 
-newData = ''
+    # Fetch last 50 played songs
+    results = sp.current_user_recently_played(limit=50)
 
-for idx, item in enumerate(results['items']):
+    # Last saved timestamp
+    f = open("last-stamp.save", "r")
+    stamp = f.read()
 
-    if  int(dateutil.parser.parse(item["played_at"]).strftime('%s')) > int(stamp):
+    # Save new data in seperate string
+    newData = ''
 
-        track = item['track']
+    for idx, item in enumerate(results['items']):
 
-        newData = newData + track['artists'][0]['name'] + " - " + track['name'] + "\n"
+        # only take songs, older then last timestamp
+        if  int(dateutil.parser.parse(item["played_at"]).strftime('%s')) > int(stamp):
 
-    if idx == 0:
-        last_stamp = item["played_at"]
+            track = item['track']
+            newData = newData + track['artists'][0]['name'] + " - " + track['name'] + "\n"
 
-with open('history.txt', 'r') as original: data = original.read()
-original.close()
-with open('history.txt', 'w') as modified: modified.write(newData + data)
-modified.close()
+        # At index zero, the last 
+        if idx == 0:
+            last_stamp = item["played_at"]
 
-# 2022-11-18T23:09:04.344Z
-d = dateutil.parser.parse(last_stamp).strftime('%s')
-f = open("last-stamp.save", "w")
-f.write(str(d))
-f.close()
+    # Write new data at beginning of existing file
+    with open('history.txt', 'r') as original: data = original.read()
+    original.close()
+    with open('history.txt', 'w') as modified: modified.write(newData + data)
+    modified.close()
+
+    # Save last time stamp from data
+    # Format out of api:
+    # 2022-11-18T23:09:04.344Z
+    d = dateutil.parser.parse(last_stamp).strftime('%s')
+    f = open("last-stamp.save", "w")
+    f.write(str(d))
+    f.close()
+
+if __name__ == '__main__':
+
+    # If somethign went wrong, write into new logfile
+    try:
+        fetch_data()
+    except e:
+        f = open('logs/' + str(datetime.now().strftime("%H:%M:%S")) +  "_error.log", "w") 
+        f.write(e)
+        f.close()
+
 
 
---
+++ src/scripts/logs/logs.md
@@ -0,0 +1,3 @@
+# Logs
+
+Save all logs in this folder for error handling, if something went wrong.
